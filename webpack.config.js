f98103790589354268232ca51810292e0aff426a
Tamas Szoke <tamas.szoke.dev@gmail.com>
2019-12-30 19:29:37 +0100

    [ADD]: Dist config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     libraryTarget: 'commonjs2'
   },
@@ -13,7 +13,7 @@ module.exports = {
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|build)/,
+        exclude: /(node_modules|dist)/,
         use: {
           loader: 'babel-loader',
           options: {
@@ -24,7 +24,7 @@ module.exports = {
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|build)/,
+        exclude: /(node_modules|dist)/,
         use: ['babel-loader', 'eslint-loader']
       }
     ]
